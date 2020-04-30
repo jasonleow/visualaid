93ae308b3ced10326507e05362561aa5430ad45e
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-04-30 18:13:30 +0800

    Added open graph

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,20 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        
+        <title key="title">VisualAid</title>,
+        <meta name="title" content="VisualAid" />,
+        <meta name="description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />,
+        <meta property="og:type" content="website" />,
+        <meta property="og:url" content="https://visualaid.netlify.app" />,
+        <meta property="og:title" content="VisualAid" />,
+        <meta property="og:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />,
+        <meta property="og:image" content="https://visualaid.netlify.app/opengraph_image.png" />,
+        <meta property="twitter:card" content="summary_large_image" />,
+        <meta property="twitter:url" content="https://visualaid.netlify.app" />,
+        <meta property="twitter:title" content="VisualAid" />,
+        <meta property="twitter:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />,
+        <meta property="twitter:image" content="https://visualaid.netlify.app/opengraph_image.png" />,
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
     ]);
 
     setPostBodyComponents([
