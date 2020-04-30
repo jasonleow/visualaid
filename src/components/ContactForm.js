381f97f53a3b5e0eadabfa98269145b2a4c41830
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-04-30 20:03:33 +0800

    Added required to all fields

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -10,17 +10,17 @@ export default class ContactForm extends React.Component {
               </p>
               <p className="form-row">
                 <label className="form-label" htmlFor="contact-user-name">Name</label>
-                <input type="text" name="name" id="contact-user-name" className="form-input" placeholder="Enter your name"/>
+                <input type="text" name="name" id="contact-user-name" className="form-input" placeholder="Enter your name" required />
                 <span className="input-focus" aria-hidden="true" />
               </p>
               <p className="form-row">
                 <label className="form-label" htmlFor="contact-user-email">Email</label>
-                <input type="email" name="email" id="contact-user-email" className="form-input" placeholder="Enter your email address"/>
+                <input type="email" name="email" id="contact-user-email" className="form-input" placeholder="Enter your email address" required />
                 <span className="input-focus" aria-hidden="true" />
               </p>
               <p className="form-row">
                 <label className="form-label" htmlFor="contact-message">Message</label>
-                <textarea name="message" id="contact-message" className="form-textarea" rows="5" placeholder="Enter your message" />
+                <textarea name="message" id="contact-message" className="form-textarea" rows="5" placeholder="Enter your message" required />
                 <span className="input-focus" aria-hidden="true" />
               </p>
               <input type="hidden" name="form-name" value="contactForm" />
