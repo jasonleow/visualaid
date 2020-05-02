c4d9dcdd5f52283cbf812411a7ee5ac3862ceb02
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-05-02 16:44:51 +0800

    Removed Google analytic scripts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,6 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        <React.Fragment>
         <title key="title">VisualAid</title>
         <meta name="title" content="VisualAid" />
         <meta name="description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />
@@ -25,18 +24,7 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
         <meta property="twitter:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />
         <meta property="twitter:image" content="https://visualaid.sg/opengraph_image.png" />
         <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-        
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165278484-1"></script>
-        
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag()(dataLayer.push(arguments);)
-            gtag('js', new Date());
-    
-            gtag('config', 'UA-165278484-1');
-        </script>
-    
-        </React.Fragment>
+
     ]);
 
     setPostBodyComponents([
