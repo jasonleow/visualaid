c3fc13ef9f139455e6a0560845f8daeb00e67709
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-05-03 20:09:27 +0800

    Update PortfolioBlock.js

--- src/components/PortfolioBlock.js
+++ src/components/PortfolioBlock.js
@@ -5,36 +5,52 @@ import {htmlToReact, getPages, Link, safePrefix} from '../utils';
 
 export default class PortfolioBlock extends React.Component {
     render() {
-        let display_projects = _.orderBy(getPages(this.props.pageContext.pages, '/projects'), 'frontmatter.date', 'desc');
+       let display_projects = _.orderBy(getPages(this.props.pageContext.pages, '/projects'), 'frontmatter.date', 'desc');
+        let recent_projects = display_projects.slice(0, _.get(this.props, 'section.num_projects_displayed'));
+        let post_len = _.size(recent_projects);
         return (
-            <Layout {...this.props}>
-            <div className="outer">
+            <section id={_.get(this.props, 'section.section_id')} className="portfolio-block block outer">
               <div className="inner">
-                <header className="page-header inner-small">
-                  <h1 className="page-title line-top">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
-                  {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
-                  <p className="page-subtitle">{_.get(this.props, 'pageContext.frontmatter.subtitle')}</p>
+                <div className="block-header">
+                  <h2 className="block-title line-top">{_.get(this.props, 'section.title')}</h2>
+                  {_.get(this.props, 'section.subtitle') && 
+                  <p className="block-subtitle">
+                    {htmlToReact(_.get(this.props, 'section.subtitle'))}
+                  </p>
                   }
-                </header>
-                <div className={'portfolio-feed layout-' + _.get(this.props, 'pageContext.frontmatter.layout_style')}>
-                  {_.map(display_projects, (post, post_idx) => (
-                  <article key={post_idx} className="post project">
-                    <Link to={safePrefix(_.get(post, 'url'))} className="post-link">
-                      {_.get(post, 'frontmatter.thumb_img_path') && 
-                      <div className="post-thumbnail">
-                        <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
-                      </div>
+                </div>
+                <div className="block-content">
+                  <div className={'portfolio-feed layout-' + _.get(this.props, 'section.layout_style')}>
+                    {
+                    _.map(recent_projects, (post, post_idx) => (
+                    <article key={post_idx} className="post project">
+                      {(post_idx === post_len - 1) ? 
+                       <Link to={_.get(this.props, 'section.view_all_url')} className="post-link">
+                        {_.get(post, 'frontmatter.thumb_img_path') && 
+                        <div className="post-thumbnail">
+                          <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
+                        </div>
+                        }
+                        <span className="view-all">{_.get(this.props, 'section.view_all_text') || 'View All'}</span>
+                      </Link>
+                       :
+                      <Link to={safePrefix(_.get(post, 'url'))} className="post-link">
+                        {_.get(post, 'frontmatter.thumb_img_path') && 
+                        <div className="post-thumbnail">
+                          <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
+                        </div>
+                        }
+                        <header className="post-header">
+                          <h3 className="post-title">{_.get(post, 'frontmatter.title')}</h3>
+                        </header>
+                      </Link>
                       }
-                      <header className="post-header">
-                        <h2 className="post-title">{_.get(post, 'frontmatter.title')}</h2>
-                      </header>
-                    </Link>
-                  </article>
-                  ))}
+                    </article>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-            </Layout>
+            </section>
         );
     }
 }
