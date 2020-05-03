bb99a80593b41976fa1929515499ba77f66ffd09
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-05-03 11:39:01 +0800

    Hide View All features - 2nd attempt

--- src/components/PortfolioBlock.js
+++ src/components/PortfolioBlock.js
@@ -25,7 +25,7 @@ export default class PortfolioBlock extends React.Component {
                     _.map(recent_projects, (post, post_idx) => (
                     <article key={post_idx} className="post project">
                       {(post_idx === post_len - 1) ? 
-                       <Link to={_.get(this.props, 'section.view_all_url')} className="post-link">
+                       {/* <Link to={_.get(this.props, 'section.view_all_url')} className="post-link">
                         {_.get(post, 'frontmatter.thumb_img_path') && 
                         <div className="post-thumbnail">
                           <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
@@ -33,7 +33,7 @@ export default class PortfolioBlock extends React.Component {
                         }
                         <span className="view-all">{_.get(this.props, 'section.view_all_text') || 'View All'}</span>
                       </Link>
-                       : 
+                       : */}
                       <Link to={safePrefix(_.get(post, 'url'))} className="post-link">
                         {_.get(post, 'frontmatter.thumb_img_path') && 
                         <div className="post-thumbnail">
