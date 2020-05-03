fa8bb1167ca77fccdf47108428f00f1fd60863f6
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-05-03 11:49:07 +0800

    Update PortfolioBlock.js

--- src/components/PortfolioBlock.js
+++ src/components/PortfolioBlock.js
@@ -25,15 +25,15 @@ export default class PortfolioBlock extends React.Component {
                     _.map(recent_projects, (post, post_idx) => (
                     <article key={post_idx} className="post project">
                       {(post_idx === post_len - 1) ? 
-                       {/* <Link to={_.get(this.props, 'section.view_all_url')} className="post-link">
+                       /*<Link to={_.get(this.props, 'section.view_all_url')} className="post-link">
                         {_.get(post, 'frontmatter.thumb_img_path') && 
                         <div className="post-thumbnail">
                           <img className="thumbnail" src={safePrefix(_.get(post, 'frontmatter.thumb_img_path'))} alt={_.get(post, 'frontmatter.title')} />
                         </div>
                         }
                         <span className="view-all">{_.get(this.props, 'section.view_all_text') || 'View All'}</span>
-                      </Link>*/}
-                       : 
+                      </Link>
+                       :*/ 
                       <Link to={safePrefix(_.get(post, 'url'))} className="post-link">
                         {_.get(post, 'frontmatter.thumb_img_path') && 
                         <div className="post-thumbnail">
