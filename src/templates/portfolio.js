cc97fc376c5a6fdfa031ee27dcbbd0549e1ab1a4
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-05-09 17:40:46 +0800

    Changed project order to ascending

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -6,7 +6,7 @@ import {getPages, Link, safePrefix} from '../utils';
 
 export default class Portfolio extends React.Component {
     render() {
-        let display_projects = _.orderBy(getPages(this.props.pageContext.pages, '/projects'), 'frontmatter.date', 'desc');
+        let display_projects = _.orderBy(getPages(this.props.pageContext.pages, '/projects'), 'frontmatter.date', 'asc');
         return (
             <Layout {...this.props}>
             <div className="outer">
