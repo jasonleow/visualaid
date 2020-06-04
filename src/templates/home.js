2c00d94d3f63bc9b20773f2ba0160368091b6152
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-06-04 17:13:29 +0800

    Update home.js

--- src/templates/home.js
+++ src/templates/home.js
@@ -8,18 +8,18 @@ export default class Home extends React.Component {
         return (
             <React.Fragment>
                 <title>VisualAid</title>
-                <meta name="title" content="VisualAid">
-                <meta name="description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19.">
-                <meta property="og:type" content="article">
-                <meta property="og:url" content="https://visualaid.sg/">
-                <meta property="og:title" content="VisualAid">
-                <meta property="og:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19.">
-                <meta property="og:image" content="https://visualaid.sg/visualaid_opengraphimage.png">
-                <meta property="twitter:card" content="summary_large_image">
-                <meta property="twitter:url" content="https://visualaid.sg/">
-                <meta property="twitter:title" content="VisualAid">
-                <meta property="twitter:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19.">
-                <meta property="twitter:image" content="https://visualaid.sg/visualaid_opengraphimage.png">
+                <meta name="title" content="VisualAid" />
+                <meta name="description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />
+                <meta property="og:type" content="article" />
+                <meta property="og:url" content="https://visualaid.sg/" />
+                <meta property="og:title" content="VisualAid" />
+                <meta property="og:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />
+                <meta property="og:image" content="https://visualaid.sg/visualaid_opengraphimage.png" />
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://visualaid.sg/" />
+                <meta property="twitter:title" content="VisualAid" />
+                <meta property="twitter:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />
+                <meta property="twitter:image" content="https://visualaid.sg/visualaid_opengraphimage.png" />
             </React.Fragment>
             <Layout {...this.props}>
             {_.map(_.get(this.props, 'pageContext.frontmatter.sections'), (section, section_idx) => {
