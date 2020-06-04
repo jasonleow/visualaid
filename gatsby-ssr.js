ae1bb14276d2905cd8157c2b290faf62317a0ec9
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2020-06-04 14:25:49 +0800

    Deleted meta tags as an experiment

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,19 +10,6 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        <title key="title">VisualAid</title>,
-        <meta name="title" content="VisualAid" />,
-        <meta name="description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />,
-        <meta property="og:type" content="article" />,
-        <meta property="og:url" content="https://visualaid.sg" />,
-        <meta property="og:title" content="VisualAid" />,
-        <meta property="og:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />,
-        <meta property="og:image" content="https://visualaid.sg/visualaid_opengraphimage.png" />,
-        <meta property="twitter:card" content="visualaid_opengraphimage.png" />,
-        <meta property="twitter:url" content="https://visualaid.sg" />,
-        <meta property="twitter:title" content="VisualAid" />,
-        <meta property="twitter:description" content="Visual aids to enable better communication between healthcare workers & migrant workers affected by COVID-19." />,
-        <meta property="twitter:image" content="https://visualaid.sg/visualaid_opengraphimage.png" />,
         <link rel="shortcut icon" type="image/png" href="/visual_aid_LOGO-02-transparent.png" />
     ]);
 
